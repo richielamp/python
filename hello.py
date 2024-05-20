31013d87f793846f1078cecfd558c24d2b83beda
richielamp <richardlampoh@gmail.com>
2024-05-20 01:48:39 +0000

    pthon script changes

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello eyram")
+x = "Rich"
+y = "Lamps"
+print("richie's surname is lampoh")
+#print(f"{x}'s surname {y}")
+print(x + "ie's surname is "+ y)
+print(x + "ie's brother Edie is here")
+print("edie told " + x + " to call " + y)
+print(f"{x}ie's surname is {y}") #with this there is no need for + and many ""

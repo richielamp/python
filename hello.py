b13265f58091a34f1dc0fae572266b0a27bef8b1
richielamp <richardlampoh@gmail.com>
2024-05-21 02:00:35 +0000

    modifying inputs and variables

--- hello.py
+++ hello.py
@@ -6,3 +6,22 @@ print(x + "ie's surname is "+ y)
 print(x + "ie's brother Edie is here")
 print("edie told " + x + " to call " + y)
 print(f"{x}ie's surname is {y}") #with this there is no need for + and many ""
+#imput
+#input("Whta is your IP address? ")
+#ip = input("what is your IP address? ")
+#print("The IP address you entered is " + ip)
+
+#print("You are targeting " + input("What IP would you like to target? "))
+
+#fname = "What is your First Name? "
+#lname = "What is your Last Name? "
+#print("Your name is " + input(fname) + " " + input(lname))
+
+#str is for strings, int is for integer 
+#both can be used with numbers depending on what the output is.
+
+print("Thanks for using this wonderful space")
+pname = input("What is your pet's name? ")
+cyt = input("Which city were you born? ")
+
+print(f"Your new twitter handle and bio is @cyber{pname} from {cyt}")
